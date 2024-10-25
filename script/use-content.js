e1380692ba65ceb9bf212f2fd091ffa0c74e38eb
annin-total <130845842+annin-total@users.noreply.github.com>
2024-10-25 11:04:56 +0900

    Update use-content.js!

--- script/use-content.js
+++ script/use-content.js
@@ -26,10 +26,51 @@ howtouseTable.addEventListener("mouseleave", () => {
 
 // (1)ホバーしている行のクラス名に「tr-[i]」を付ける。背景画像を変える。
 // (2)ホバーしている行以外のすべての行のtdのクラス名にfadeをつける。
+// rows.forEach((row, index) => {
+//   row.addEventListener("mouseenter", () => {
+//     howtouseContainer.style.backgroundImage = `url('${howtouseImages[index]}')`;
+//     howtouseContainer.style.opacity = "1";
+//     row.classList.add(`tr-${index + 1}`);
+
+//     // 他の行のテキストを薄くする
+//     if (index !== 0 && index !== 7) {
+//       rows.forEach((otherRow, otherIndex) => {
+//         if (otherIndex !== index) {
+//           otherRow
+//             .querySelectorAll("td")
+//             .forEach((cell) => cell.classList.add("fade"));
+//         }
+//       });
+//     }
+//   });
+
+//   row.addEventListener("mouseleave", () => {
+//     row.classList.remove(`tr-${index + 1}`);
+
+//     // 全ての行のテキストの透明度を元に戻す;
+//     rows.forEach((otherRow) => {
+//       otherRow
+//         .querySelectorAll("td")
+//         .forEach((cell) => cell.classList.remove("fade"));
+//     });
+//   });
+// });
+
+// テスト
 rows.forEach((row, index) => {
-  row.addEventListener("mouseenter", () => {
-    howtouseContainer.style.backgroundImage = `url('${howtouseImages[index]}')`;
-    howtouseContainer.style.opacity = "1";
+  row.addEventListener("mouseenter", async () => {
+    const imageUrl = howtouseImages[index];
+    const img = new Image();
+
+    // 画像の読み込みが完了したら背景画像を設定
+    img.onload = () => {
+      howtouseContainer.style.backgroundImage = `url('${imageUrl}')`;
+      howtouseContainer.style.opacity = "1";
+    };
+
+    // 画像の読み込みを開始
+    img.src = imageUrl;
+
     row.classList.add(`tr-${index + 1}`);
 
     // 他の行のテキストを薄くする
